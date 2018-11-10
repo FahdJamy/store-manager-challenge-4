d383931313db3073235dda74773b99e34ff7000a
muhwezideo <fahdjamy2@gmail.com>
2018-11-10 21:14:02 +0300

    [Deliver #161859370] store a token and a username in a local storage

--- js/login.js
+++ js/login.js
@@ -15,15 +15,21 @@ loginButton.addEventListener('click', (e) => {
 	api.login(login_url, user_data)
 	.then(data => {
 		if (data['message'] === 'sorry username and password dont match, please login with valid credentials') {
-			window.location.href = 'index.html';
 			// console.log(data);
 		} else if (data['message'] === `Sorry user ${user.value} doesnot exist, login with valid credentials`) {
-			window.location.href = 'index.html';
 			// console.log(data);
 		} else if (user.value === 'admin' && passwrd.value === '123') {
+			token = data['token']
+			username = user.value;
+			localStorage.setItem('token',token);
+			localStorage.setItem('username',username);
 			window.location.href = 'admin.html';
-			console.log(data);
+			// console.log(data);
 		} else {
+			token = data['token']
+			username = user.value;
+			localStorage.setItem('token',token);
+			localStorage.setItem('username',username);
 			window.location.href = 'home.html';
 			// console.log(data);
 		}
