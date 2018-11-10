6f373ea2e44bde8d93a3f4e99cab0f6d8dc7686c
muhwezideo <fahdjamy2@gmail.com>
2018-11-10 20:34:09 +0300

    [Deliver #161859370] modify login method. run to check if it can post

--- js/login.js
+++ js/login.js
@@ -1,14 +1,26 @@
+const api = new API;
+var login_url = 'https://store-manager-challenge-3.herokuapp.com/api/v2/auth/login';
+
 const user = document.getElementById('username');
 const passwrd = document.getElementById('pass');
 const loginButton = document.getElementById('loginBtn');
 
-loginButton.addEventListener('click', (e) => {
 
-	if (user.value === 'admin' && passwrd.value === '123') {
-		window.location.href = 'admin.html';
-	} else {
-		window.location.href = 'home.html';
+loginButton.addEventListener('click', (e) => {
+	var user_data = {
+		username:user.value,
+		password:passwrd.value
 	}
+
+	api.login(login_url, user_data)
+	.then(data => console.log(data))
+	.catch( error => console.log(error));
+
+	// if (user.value === 'admin' && passwrd.value === '123') {
+	// 	window.location.href = 'admin.html';
+	// } else {
+	// 	window.location.href = 'home.html';
+	// }
 
 	e.preventDefault();
 });
