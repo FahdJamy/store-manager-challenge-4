4afb4f2c591970ad662d5bb6b292c580e9ed7f06
muhwezideo <fahdjamy2@gmail.com>
2018-11-11 17:47:29 +0300

    [Deliver #161859412] add method to validate user entry values before creation of a new sales attendant account

--- js/signup.js
+++ js/signup.js
@@ -3,21 +3,42 @@ var signup_url = 'https://store-manager-challenge-3.herokuapp.com/api/v2/auth/si
 var token = localStorage.getItem("token");
 
 const new_username = document.getElementById('username');
-const phone = document.getElementById('phone_no');
+const user_phone_no = document.getElementById('phone_no');
 const passcode = document.getElementById('pass');
 const confirm_password = document.getElementById('c_pass');
 const signupButton = document.getElementById('create_user');
 const msgBody = document.getElementById('invalid')
 
 signupButton.addEventListener('click', (e) => {
-	console.log('created')
-	var user_data = {
-		username:new_username.value,
-		phone_no:parseInt(phone.value),
-		password:passcode.value
+	if (new_username.value === ""|| user_phone_no.value === "" || passcode.value === "") {
+		msgBody.innerHTML = 'sorry all fields must be filled in';
+	} else if (confirm_password.value !== passcode.value) {
+		msgBody.innerHTML = 'password and confirm password must be equal';
+		// console.log(new_username.value)
+		// console.log(user_phone_no.value)
+		// console.log(passcode.value)
+	} else {
+		var user_data = {
+			username:new_username.value,
+			phone_no:parseInt(user_phone_no.value),
+			password:passcode.toString()
+		}
+		api.post(signup_url, user_data, token)
+			.then(data => {
+				if (data['message'] === 'sorry username is already taken') {
+					msgBody.innerHTML = 'sorry that username is already taken';
+					msgBody.className = "danger_txt";
+				}  else if (data['message'] === 'sorry username shouldnt have a special character including ($#@%)') {
+					msgBody.innerHTML = `${data['message']}`;
+				} else if (data['message'] === 'User succefully registered') {
+					msgBody.innerHTML = 'User account succefully created';
+					msgBody.className = "success_txt";
+				}
+				console.log(data)
+			})
+			.catch(error => {
+				msgBody.innerHTML = 'We are experiencing some difficulties now, please try again later';
+				console.log(error)
+			})
 	}
-	console.log(token)
-	api.post(signup_url, user_data, token)
-		.then(data => console.log(data))
-		.catch(error => console.log(error))
 });
