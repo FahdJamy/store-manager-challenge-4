8b0d21daf82c521a980f71945d2d9aab7bf7e318
muhwezideo <fahdjamy2@gmail.com>
2018-11-12 17:00:39 +0300

    [Deliver #161859422] add an if statement to check for wether all fields are input with data

--- js/product.js
+++ js/product.js
@@ -1,5 +1,6 @@
 const api = new API;
 const create_product_url = 'https://store-manager-challenge-3.herokuapp.com/api/v2/categories'
+var token = localStorage.getItem("token");
 
 const pdt_name = document.getElementById('c_product');
 const pdt_price = document.getElementById('p_price');
@@ -11,5 +12,20 @@ const msgBody = document.getElementById('message')
 
 sndButton.addEventListener('click', (e) => {
 	console.log('ready')
+	var productInfo = {
+		name:String(pdt_name.value),
+		category:String(category.value),
+		quantity:parseInt(quantity.value),
+		price:parseInt(pdt_price.value)
+	}
+	if (pdt_name.value === "" || category.value === "" || quantity.value === "" || pdt_price.value === "") {
+			msgBody.innerHTML = 'please first fill in all fields before creating a new product';
+		}
+	api.post(create_product_url, productInfo, token)
+		.then((data) => console.log(data))
+		.catch( error => {
+			console.log(error)
+			msgBody.innerHTML = 'sorry we having problems now, please try again later'
+		});
 	e.preventDefault();
 })
