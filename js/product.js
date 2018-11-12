18498e09cbeba972159d901557258db95b51b379
muhwezideo <fahdjamy2@gmail.com>
2018-11-12 16:34:52 +0300

    [Deliver #161859422] create all required variables for creating a new product in product.js file plus modify the admin.html and records.css files

---
+++ js/product.js
@@ -0,0 +1,10 @@
+const api = new API;
+const create_product_url = 'https://store-manager-challenge-3.herokuapp.com/api/v2/categories'
+
+const pdt_name = document.getElementById('c_product');
+const pdt_price = document.getElementById('p_price');
+const quantity = document.getElementById('stock');
+const category = document.getElementById('categoryName');
+
+const sndButton = document.getElementById('createPdt');
+const msgBody = document.getElementById('message')
