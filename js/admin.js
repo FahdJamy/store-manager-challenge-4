34a583e695115824d25d043b22e7a3fcb4f6d4c4
muhwezideo <fahdjamy2@gmail.com>
2018-11-13 11:49:05 +0300

    add js linting librabry JsPrettier

--- js/admin.js
+++ js/admin.js
@@ -1,56 +1,55 @@
-let closBtn = document.getElementById('closure');
-let openBtn = document.getElementById('updateBtn');
-let openBtn2 = document.getElementById('updateBtn2');
-let openBtn3 = document.getElementById('updateBtn3');
-let openBtn4 = document.getElementById('updateBtn4');
-let openBtn5 = document.getElementById('updateBtn5');
-let openBtn6 = document.getElementById('updateBtn6');
-let openBtn7 = document.getElementById('updateBtn7');
-let openBtn8 = document.getElementById('updateBtn8');
-let modalOpen = document.getElementById('update_modal');
+let closBtn = document.getElementById("closure");
+let openBtn = document.getElementById("updateBtn");
+let openBtn2 = document.getElementById("updateBtn2");
+let openBtn3 = document.getElementById("updateBtn3");
+let openBtn4 = document.getElementById("updateBtn4");
+let openBtn5 = document.getElementById("updateBtn5");
+let openBtn6 = document.getElementById("updateBtn6");
+let openBtn7 = document.getElementById("updateBtn7");
+let openBtn8 = document.getElementById("updateBtn8");
+let modalOpen = document.getElementById("update_modal");
 
-closBtn.addEventListener('click', closeWindow);
-openBtn.addEventListener('click', openWindow);
-openBtn2.addEventListener('click', openWindow);
-openBtn3.addEventListener('click', openWindow);
-openBtn4.addEventListener('click', openWindow);
-openBtn5.addEventListener('click', openWindow);
-openBtn6.addEventListener('click', openWindow);
-openBtn7.addEventListener('click', openWindow);
-openBtn8.addEventListener('click', openWindow);
-window.addEventListener('click', outSideClose);
+closBtn.addEventListener("click", closeWindow);
+openBtn.addEventListener("click", openWindow);
+openBtn2.addEventListener("click", openWindow);
+openBtn3.addEventListener("click", openWindow);
+openBtn4.addEventListener("click", openWindow);
+openBtn5.addEventListener("click", openWindow);
+openBtn6.addEventListener("click", openWindow);
+openBtn7.addEventListener("click", openWindow);
+openBtn8.addEventListener("click", openWindow);
+window.addEventListener("click", outSideClose);
 
 //// Close modal view on click of "x" close btn ////
-function closeWindow () {
-	modalOpen.style.display = 'none';
+function closeWindow() {
+	modalOpen.style.display = "none";
 }
 //// Open modal view on click of "View detail" btn ////
-function openWindow () {
-	modalOpen.style.display = 'block';
+function openWindow() {
+	modalOpen.style.display = "block";
 }
 
 // Add product
-let addBtn = document.getElementById('create_pdt');
-let addModal = document.getElementById('add_modal');
-let closAddModal = document.getElementById('closeModalAdd');
+let addBtn = document.getElementById("create_pdt");
+let addModal = document.getElementById("add_modal");
+let closAddModal = document.getElementById("closeModalAdd");
 
-addBtn.addEventListener('click', addProduct);
-closAddModal.addEventListener('click', closeModalAdd);
+addBtn.addEventListener("click", addProduct);
+closAddModal.addEventListener("click", closeModalAdd);
 
-function addProduct () {
-	addModal.style.display = 'block';
+function addProduct() {
+	addModal.style.display = "block";
 }
 
-function closeModalAdd () {
-	addModal.style.display = 'none';
+function closeModalAdd() {
+	addModal.style.display = "none";
 }
-
 
 //// Close modal view on outside click of modal view ////
-function outSideClose (e) {
+function outSideClose(e) {
 	if (e.target === modalOpen) {
-		modalOpen.style.display = 'none';
+		modalOpen.style.display = "none";
 	} else if (e.target === addModal) {
-		addModal.style.display = 'none';
+		addModal.style.display = "none";
 	}
 }
--- js/signup.js
+++ js/signup.js
@@ -1,45 +1,55 @@
-const api = new API;
-var signup_url = 'https://store-manager-challenge-3.herokuapp.com/api/v2/auth/signup';
+const api = new API();
+var signup_url =
+	"https://store-manager-challenge-3.herokuapp.com/api/v2/auth/signup";
 var token = localStorage.getItem("token");
 
-const new_username = document.getElementById('username');
-const user_phone_no = document.getElementById('phone_no');
-const passcode = document.getElementById('pass');
-const confirm_password = document.getElementById('c_pass');
-const signupButton = document.getElementById('create_user');
-const msgBody = document.getElementById('invalid')
+const new_username = document.getElementById("username");
+const user_phone_no = document.getElementById("phone_no");
+const passcode = document.getElementById("pass");
+const confirm_password = document.getElementById("c_pass");
+const signupButton = document.getElementById("create_user");
+const msgBody = document.getElementById("invalid");
 
-signupButton.addEventListener('click', (e) => {
-	if (new_username.value === ""|| user_phone_no.value === "" || passcode.value === "") {
-		msgBody.innerHTML = 'sorry all fields must be filled in';
+signupButton.addEventListener("click", e => {
+	if (
+		new_username.value === "" ||
+		user_phone_no.value === "" ||
+		passcode.value === ""
+	) {
+		msgBody.innerHTML = "sorry all fields must be filled in";
 	} else if (confirm_password.value !== passcode.value) {
-		msgBody.innerHTML = 'password and confirm password must be equal';
-	} else if ((passcode.value).length < 5 || (user_phone_no.value).length < 5) {
-		msgBody.innerHTML = 'both phone Nummber and password should atleast be 6 charcters more';
-	} else if ((new_username.value).length < 3) {
-		msgBody.innerHTML = 'username should be atleast 3 characters';
+		msgBody.innerHTML = "password and confirm password must be equal";
+	} else if (passcode.value.length < 5 || user_phone_no.value.length < 5) {
+		msgBody.innerHTML =
+			"both phone Nummber and password should atleast be 6 charcters more";
+	} else if (new_username.value.length < 3) {
+		msgBody.innerHTML = "username should be atleast 3 characters";
 	} else {
 		var user_data = {
-			username:(new_username.value).toString(),
-			password:String(passcode.value),
-			phone_no:parseInt(user_phone_no.value)
-		}
+			username: new_username.value.toString(),
+			password: String(passcode.value),
+			phone_no: parseInt(user_phone_no.value)
+		};
 		api.post(signup_url, user_data, token)
 			.then(data => {
-				if (data['message'] === 'sorry username is already taken') {
-					msgBody.innerHTML = 'sorry that username is already taken';
+				if (data["message"] === "sorry username is already taken") {
+					msgBody.innerHTML = "sorry that username is already taken";
 					msgBody.className = "danger_txt";
-				}  else if (data['message'] === 'sorry username shouldnt have a special character including ($#@%)') {
-					msgBody.innerHTML = `${data['message']}`;
-				} else if (data['message'] === 'User succefully registered') {
-					msgBody.innerHTML = 'User account succefully created';
+				} else if (
+					data["message"] ===
+					"sorry username shouldnt have a special character including ($#@%)"
+				) {
+					msgBody.innerHTML = `${data["message"]}`;
+				} else if (data["message"] === "User succefully registered") {
+					msgBody.innerHTML = "User account succefully created";
 					msgBody.className = "success_txt";
 				}
-				console.log(data)
+				console.log(data);
 			})
 			.catch(error => {
-				msgBody.innerHTML = 'We are experiencing some difficulties now, please try again later';
-				console.log(error)
-			})
+				msgBody.innerHTML =
+					"We are experiencing some difficulties now, please try again later";
+				console.log(error);
+			});
 	}
 });
