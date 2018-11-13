9c267744d34d35b7c4d333d36b1c10af4dd1ec60
muhwezideo <fahdjamy2@gmail.com>
2018-11-13 12:12:35 +0300

    [Deliver #161859422] add method to check for invalid user inputs

--- js/product.js
+++ js/product.js
@@ -1,5 +1,5 @@
 const api = new API;
-const create_product_url = 'https://store-manager-challenge-3.herokuapp.com/api/v2/categories'
+const create_product_url = 'https://store-manager-challenge-3.herokuapp.com/api/v2/products'
 var token = localStorage.getItem("token");
 
 const pdt_name = document.getElementById('c_product');
@@ -10,19 +10,32 @@ const category = document.getElementById('categoryName');
 const sndButton = document.getElementById('createPdt');
 const msgBody = document.getElementById('message')
 
+// Capitalize category name and product name first
+
 sndButton.addEventListener('click', (e) => {
+	var productName = String(pdt_name.value.charAt(0).toUpperCase() + pdt_name.value.slice(1));
+	var category_name = String(category.value.charAt(0).toUpperCase() + category.value.slice(1));
 	console.log('ready')
 	var productInfo = {
-		name:String(pdt_name.value),
-		category:String(category.value),
+		name:productName,
+		category:category_name,
 		quantity:parseInt(quantity.value),
 		price:parseInt(pdt_price.value)
 	}
 	if (pdt_name.value === "" || category.value === "" || quantity.value === "" || pdt_price.value === "") {
 			msgBody.innerHTML = 'please first fill in all fields before creating a new product';
+		} else if (pdt_name.value.length < 2 || category.value.length < 2) {
+			msgBody.innerHTML = 'product name and category should atleast be 3 characters';
 		}
 	api.post(create_product_url, productInfo, token)
-		.then((data) => console.log(data))
+		.then((data) => {
+			console.log(data)
+			if (data['message'] === `category name ${category_name} doesnot exist`) {
+				msgBody.innerHTML = `${data['message']}`;
+			} else if (data['message'] === `sorry product with name ${productName} already exists`) {
+				msgBody.innerHTML = `${data['message']}`;
+			}
+		})
 		.catch( error => {
 			console.log(error)
 			msgBody.innerHTML = 'sorry we having problems now, please try again later'
