3c4df47726d9ec093e8eb0a9feffe3519ae80887
muhwezideo <fahdjamy2@gmail.com>
2018-11-19 15:37:17 +0300

    [Deliver #162006949] add logic to display informative infomation if user input is wrong or when a new sales record has been created

--- js/records.js
+++ js/records.js
@@ -4,13 +4,15 @@ var recordBtn = document.getElementById("recordBtn");
 var modalOpen = document.getElementById("modal");
 var token = localStorage.getItem("token");
 var sales_url = "https://store-manager-challenge-3.herokuapp.com/api/v2/sales";
+var createSaleUrl =
+	"https://store-manager-challenge-3.herokuapp.com/api/v2/sales";
 
-var createSaleRec = document.getElementById('createSaleBtn');
+var createSaleRec = document.getElementById("createSaleBtn");
 
 closBtn.addEventListener("click", closeWindow);
 recordBtn.addEventListener("click", openWindow);
 window.addEventListener("click", outSideClose);
-createSaleRec.addEventListener('click', createSalesRecord);
+createSaleRec.addEventListener("click", createSalesRecord);
 
 //// Close modal view on click of "x" close btn ////
 function closeWindow() {
@@ -66,15 +68,60 @@ api.get_token(sales_url, token)
 	})
 	.catch(error => console.log(error));
 
-
 ///// Create new sales record
-function createSalesRecord (e) {
-	var product = document.getElementById('product').value;
-	var quantitySold = document.getElementById('qnty').value;
-	var messagee = document.getElementById('mesg');
+function createSalesRecord(e) {
+	var product = document.getElementById("product").value;
+	var quantitySold = document.getElementById("qnty").value;
+	var messagee = document.getElementById("mesg");
 	if (product === "" && quantitySold === "") {
 		messagee.innerHTML = `Sorry, all fields are required.`;
-		messagee.className = 'msgInfo';
+		messagee.className = "msgInfo";
+	} else {
+		var data = {
+			name: String(product.charAt(0).toUpperCase() + product.slice(1)),
+			quantity: parseInt(quantitySold)
+		};
+		api.post(createSaleUrl, data, token)
+			.then(response => {
+				if (
+					response["message"] ===
+					"sorry an admin cant create a sales record"
+				) {
+					localStorage.removeItem("token");
+					localStorage.removeItem("username");
+					window.location.href = "index.html";
+					alert("Log in as a sales attendant first");
+				} else if (
+					response["message"] ===
+					"sorry, You provided an invalid token"
+				) {
+					localStorage.removeItem("token");
+					localStorage.removeItem("username");
+					window.location.href = "index.html";
+					alert("Log in as a sales attendant first");
+				} else if (
+					response["message"] ===
+					"sorry cant make a sale record of a product that doesnt exist in the db"
+				) {
+					messagee.innerHTML = `${response["message"]}`;
+					messagee.className = "msgInfo";
+				} else if (
+					response["message"] === "sorry, product is out of stock"
+				) {
+					messagee.innerHTML = `${response["message"]}`;
+					messagee.className = "msgInfo";
+				} else if (
+					response["message"] ===
+					"cant make sale, current_stock is less than provided quantity"
+				) {
+					messagee.innerHTML = `${response["message"]}`;
+					messagee.className = "msgInfo";
+				} else if (response["message"] === "sale record created") {
+					messagee.innerHTML = `${response["message"]}`;
+					messagee.className = "msgInfoSuccess";
+				}
+			})
+			.catch(error => console.log(error));
 	}
 	e.preventDefault();
-};
+}
