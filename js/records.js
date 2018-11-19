03265e4676e878a9b2d6975bfef0b2a3f3e8a300
muhwezideo <fahdjamy2@gmail.com>
2018-11-19 12:34:11 +0300

    [Deliver #162044467] write methods to fetch and display sales records data to the UI

--- js/records.js
+++ js/records.js
@@ -24,8 +24,43 @@ function outSideClose (e) {
 const api = new API;
 var token = localStorage.getItem("token");
 var sales_url = 'https://store-manager-challenge-3.herokuapp.com/api/v2/sales'
-var salesContainer = document.getElementById('records')
+
 
 api.get_token(sales_url, token)
-	.then(data => console.log(data))
+	.then(data => {
+		const salesContainer = document.getElementById('salesRecs')
+		var salesRecords = data['sales'];
+		for (var i = 0; i < salesRecords.length; i++) {
+
+			var catName = salesRecords[i]['category'];
+			var createdBy = salesRecords[i]['created_by'];
+			var dateOn = salesRecords[i]['created_on'];
+			var pdtPrice = salesRecords[i]['price'];
+			var pdtName = salesRecords[i]['product_name'];
+			var qnty = salesRecords[i]['quantity'];
+			var revenue = salesRecords[i]['total_amount'];
+			salesContainer.innerHTML += `<div class="record">
+					<img src="img/timber.jpg" alt="alt_name" class="item_img">
+					<div class="rec">
+						<h3>Timberland</h3>
+						<div class="record_info">
+							<span>Category</span>
+							<span>${pdtName}</span>
+							<span>Category</span>
+							<span>${catName}</span>
+							<span>Price</span>
+							<span>${pdtPrice} a piece</span>
+							<span>Quantiy</span>
+							<span>${qnty}</span>
+							<span>Date</span>
+							<span>${dateOn}</span>
+							<span>Total Amount Generated</span>
+							<span>${revenue}</span>
+							<span>Created By</span>
+							<span>${createdBy}</span>
+						</div>
+					</div>
+				</div>`;
+		}
+	})
 	.catch((error) => console.log(error))
